aec337084da6c3b8756cba65e9449e06abbe9b1b
Ryan Hourihan <rhourihan36@gmail.com>
2017-05-04 17:55:08 -0400

    Build Test
    tests if the android studio project can build

---
+++ app/src/testDebug/aidl/buildTest.java
@@ -0,0 +1,15 @@
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+/**
+ * Created by Acer on 5/4/2017.
+ */
+public class buildTest {
+    @Test
+    //This test will fail if the app cannot be built
+    public void main() throws Exception {
+
+    }
+
+}
