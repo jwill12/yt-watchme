9b6dfd4f59e69d487ae37d29a333a00fac0ed410
jwill12 <jeanmariewilbertiny@gmail.com>
2017-05-04 23:24:51 -0400

    finalFiles
    add in comments to where revisions where made
    tests should be under src

---
+++ testDebug/aidl/com/google/android/apps/watchme/MainActivityTest.java
@@ -0,0 +1,20 @@
+package com.google.android.apps.watchme;
+
+import junit.framework.TestCase;
+
+/**
+ * Created by Wilbertiny on 5/4/17.
+ */
+public class MainActivityTest extends TestCase {
+
+    //test will fail if app cant resume
+    public void testOnResume() throws Exception {
+
+    }
+
+    //test will fail if service is unavaiable
+    public void testShowGooglePlayServicesAvailabilityErrorDialog() throws Exception {
+
+    }
+
+}
